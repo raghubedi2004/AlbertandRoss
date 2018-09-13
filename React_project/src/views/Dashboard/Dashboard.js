02cc2b91ef15f3352a21a9d9033ba6a072ade8fe
raghubedi <42847041+raghubedi@users.noreply.github.com>
2018-09-13 15:11:55 -0400

    Update Dashboard.js

--- React_project/src/views/Dashboard/Dashboard.js
+++ React_project/src/views/Dashboard/Dashboard.js
@@ -1,13 +1,23 @@
 import React, {Component} from 'react';
+import Iframe from 'react-iframe';
 var __html = require('./TELUS.html');
 var template = { __html: __html };
 
 class Test extends Component {
 
+    componentDidMount() {
+      var ifrm = document.getElementById('buyIframe');
+      ifrm = ifrm.contentWindow || ifrm.contentDocument.document || ifrm.contentDocument;
+      ifrm.document.open();
+      ifrm.document.write(__html);
+      ifrm.document.close();
+    }
 	  render() {
 
     return (
-      <div dangerouslySetInnerHTML={template} />
+      <div>
+      <iframe id="buyIframe" src="./TELUS.html" styles={{width: "100%"}}></iframe>
+      </div>
 
     );
   }
