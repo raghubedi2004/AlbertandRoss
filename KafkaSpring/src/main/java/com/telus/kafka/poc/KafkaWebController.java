7958f9bc2c84305a7fa0dd1636f0d7229fc6285a
Alwin Anthony Tiwari <alwin_anthony.tiwari@telus.com>
2018-09-20 14:26:47 -0400

    Update KafkaWebController.java

--- KafkaSpring/src/main/java/com/telus/kafka/poc/KafkaWebController.java
+++ KafkaSpring/src/main/java/com/telus/kafka/poc/KafkaWebController.java
@@ -1,7 +1,5 @@
 package com.telus.kafka.poc;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -9,10 +7,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.mongodb.MongoClient;
-import com.telus.mongodb.dao.MongoDBSubscriberDAO;
-import com.telus.mongodb.model.SubscriberInfo;
 
 @Controller
 @CrossOrigin
@@ -23,20 +17,11 @@ public class KafkaWebController {
 
     @PostMapping("/kafka/{topicName}")
     @ResponseBody
-    public List<SubscriberInfo> sendToTopic(@PathVariable String topicName, @RequestBody RequestDTO requestDTO) {
+    public ResponseDTO sendToTopic(@PathVariable String topicName, @RequestBody RequestDTO requestDTO) {
         kafkaSender.send(topicName, requestDTO);
         ResponseDTO responseDTO = new ResponseDTO();
         responseDTO.setStatus("Message sent");
-        
-        MongoClient mongoClient = new MongoClient();
-
-        MongoDBSubscriberDAO subscriberDAO = new MongoDBSubscriberDAO(mongoClient);
-		
-		List<SubscriberInfo> subscribers = subscriberDAO.readAllSubscriberInfos();
-		
-		mongoClient.close();
-		
-        return subscribers;
+        return responseDTO;
     }
 
 }
