9be2f8b206461bfa3344bf84b435d5758dfe12b4
raghubedi <42847041+raghubedi@users.noreply.github.com>
2018-09-20 15:24:26 -0400

    Update config.js

--- React_project/src/config.js
+++ React_project/src/config.js
@@ -1,5 +1,18 @@
 const config = {
-    baseURL: 'http://127.0.0.1:9090' 
+    baseURL: 'http://d011728:9090',
+    subscribersPageUrl: '/#/telus/subscribers',
+    kafkaBaseUrl: 'http://d011728:8585',
+    ratePlanMap: {
+      'XPPDCPTRV': 'Corporate traveller 105 PDA',
+      'XPAT30U': 'Corp Advantage UL 30',
+      'XPAVDU52P': 'Corp Adv V&D wUS 52 PDA',
+      'XPAT55P1P': 'Corp Advantage VD 55 1GB PDA',
+      'XPCASH25': 'Corp Adv Smart Hub V&D 25',
+      'FNCP20D': 'Corporate 20/200',
+      'XPAT28US': 'Corp Advantage UL w/US 28',
+      'PAR11X': 'ALPHA ACCESS - RENT Corp $11',
+      'PKNF': 'FEAT -Num Access Ntwk No.'
+    }
 };
- 
+
 export default config;
