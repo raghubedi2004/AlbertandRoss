10271743305615c924f689ace0a75af9aa1dccc2
Alwin Anthony Tiwari <alwin_anthony.tiwari@telus.com>
2018-09-20 14:27:22 -0400

    Update MongoDBSubscriberDAO.java

--- KafkaSpring/src/main/java/com/telus/mongodb/dao/MongoDBSubscriberDAO.java
+++ KafkaSpring/src/main/java/com/telus/mongodb/dao/MongoDBSubscriberDAO.java
@@ -1,18 +1,11 @@
 package com.telus.mongodb.dao;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.bson.types.ObjectId;
 
 import com.mongodb.BasicDBObject;
 import com.mongodb.DBCollection;
-import com.mongodb.DBCursor;
-import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 import com.telus.kafka.poc.RequestDTO;
-import com.telus.mongodb.converter.SubscriberInfoConverter;
-import com.telus.mongodb.model.SubscriberInfo;
 
 public class MongoDBSubscriberDAO {
 
@@ -33,23 +26,9 @@ public class MongoDBSubscriberDAO {
 			BasicDBObject searchQuery = new BasicDBObject().append("_id", new ObjectId(subDBId));
 	
 			col.update(searchQuery, newDocument);
-			
-			col.save(newDocument);
 		
 		}
 		
-	}
-	
-	public List<SubscriberInfo> readAllSubscriberInfos() {
-		
-		List<SubscriberInfo> data = new ArrayList<SubscriberInfo>();
-		DBCursor cursor = col.find();
-		while (cursor.hasNext()) {
-			DBObject doc = cursor.next();
-			SubscriberInfo p = SubscriberInfoConverter.toSubscribers(doc);
-			data.add(p);
-		}
-		return data;
 	}
 
 }
